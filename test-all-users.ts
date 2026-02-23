f5ad22b08c41ea4029d8ea1ae8cd65324c01c033
winter-jackson <winterjacksonwj@gmail.com>
2026-02-23 07:01:01 +0300

    chore: remove test scripts

--- test-all-users.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-
-async function check() {
-  const users = await prisma.user.findMany();
-  console.log("EVERY SINGLE USER IN DB:");
-  console.log(users.map(u => ({ email: u.email, role: u.role, deletedAt: u.deletedAt })));
-}
-check().finally(() => prisma.$disconnect());
